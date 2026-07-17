d526451d053f65e673f94bbe104c86146f893663
EliLevasseur <eliplevasseur7@gmail.com>
2026-07-17 14:35:57 -0700

    Worked on some testing and combination function

--- main.cpp
+++ main.cpp
@@ -3,11 +3,26 @@
 #include <vector>
 #include "dataframe.h"
 
+
+struct Node {
+    std::vector<double> inputs;
+    double output;
+};
+
 // Create a data type to representa layer of neurons
 struct Layer {
     std::vector<std::vector<double>> weights;
     std::vector<double> biases;
 };
+
+// KIND OF ERROR BUT I NEED TO MAKE IT OUTPUT THE OUTPUT FOR ONE ROW OF THE CSV NOT THE ENTIRE CSV FILE
+double feedForward(std::vector<double>& inputs, Layer& layer, int weightIndex) {
+    double output = 0.0;
+    for (int i = 0; i < inputs.size(); i++) {
+        output += inputs[i] * layer.weights[weightIndex][i];
+    }
+    return output + layer.biases[weightIndex];
+}
 
 Layer createLayer(int inputs, int nodes) {
     Layer layer;
@@ -23,6 +38,8 @@ Layer createLayer(int inputs, int nodes) {
     }
     return layer;
 }
+
+//nodeMapping
 
 double sigmoid(double x) {
     x = 1.0/(1.0 + std::exp(-x));
@@ -53,33 +70,36 @@ std::vector<std::vector<double>> hiddenLayer(
     return outputMatrix;
 }
 
-
-void Activation(std::vector<double>&arr) {
-    for (auto& x: arr) {
-        x = sigmoid(x);
-    }
-}
-
-int main() {  
+int main() { 
+    //csv file turned to matrix of vectors of doubles
     std::vector<std::vector<double>> dataFrame = dataframe("data.csv");
-    std::vector<Layer> hiddenLayers;
-    std::vector<int> hiddenLayerSizes = {2, 3, 3, 4};
-    const Layer test_layer = createLayer(3, 5);
-
-    hiddenLayers.resize(hiddenLayerSizes.size());
-    
-    for (const int& x: hiddenLayerSizes) {
-        hiddenLayers.push_back(createLayer(dataFrame[0].size(), x));
+    //create a target vector
+    std::vector<double> targets;
+    targets.reserve(dataFrame.size());
+    for (int i = 0; i < dataFrame.size(); i++) {
+        targets.push_back(dataFrame[i].back());
+        dataFrame[i].pop_back();
     }
+    // IDEA IN DEV !! std::vector<int> hiddenLayerSizes = {2, 3, 3, 4};
+    Layer test_layer = createLayer(4, 2);
+    //Output of each hidden layer to be passed to the next layer as input
+    std::vector<double> hiddenLayerOutputs;
 
-  //===============Activation test==========/
-  std::vector<double> arr = {1.9, 3.1, 2.4};   
-  Activation(arr);
-  for (const auto& x: arr) {
-    std::cout << x << " ";
+  //===============Feedforward Test==========//
+  std::cout << std::endl << "Feedforward Test" << std::endl;
+  for (auto& row: dataFrame) {
+    for (std::size_t index = 0; index < test_layer.weights.size(); ++index) {
+        hiddenLayerOutputs.push_back(feedForward(row, test_layer, index));
+    }
   }
-  std::cout << "\n";
 
+
+  //check the output of the feedforward function
+  std::cout << "Feedforward Output:" << std::endl;
+  for (const auto& val: hiddenLayerOutputs) {
+      std::cout << val << " ";
+  }
+  std::cout << std::endl;
 
   //===============Layer creation Test==========//
   std::cout << std::endl << "Layer creation Test" << std::endl;
@@ -95,12 +115,17 @@ int main() {
   }
   //===============File Read test==========//
   std::cout << "File Read test" << std::endl;
+  std::cout << "Predictors" << std::endl;
   for (const auto& row: dataFrame) {
       for (const auto& val: row) {
           std::cout << val << " ";
       }
       std::cout << std::endl;
   }
+  std::cout << "Targets" << std::endl;
+  for (const auto& val: targets) {
+        std::cout << val << std::endl;
+    }
 
   //===============Hidden Layer Testt==========//
   std::cout << "Hidden Layer Test" << std::endl;
